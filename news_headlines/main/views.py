806b780d27d976599672cb3c6eb37f1fb109dee9
PyPranav <59741576+PyPranav@users.noreply.github.com>
2020-08-28 09:05:00 +0530

    asdas

---
+++ news_headlines/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from newsapi import NewsApiClient
+import json
+
+API_KEY = 'ENTER YOUR API KEY'
+
+# Create your views here.
+def news(requests):
+    newsapi = NewsApiClient(api_key=API_KEY)
+
+    top_headlines = newsapi.get_top_headlines(sources='google-news-in, the-hindu ,bbc-news, the-verge, techcrunch')
+    articles = top_headlines['articles']
+    with open('static/news.json', 'w') as f:
+        json.dump(articles, f)
+    return render(requests, 'news.html')
---
+++ news_headlines/static/news.js
@@ -0,0 +1,39 @@
+fetch('../static/news.json')
+.then(data => data.json())
+.then(Data => {
+    str=''
+    console.log(Data.length);
+    for (let z=0; z<Data.length; z++)
+    {
+        data = Data[z];
+        str += `
+                <div class="card post bg-primary shadow-soft border-light my-3" target="_blank" onclick="window.open('${data.url}')">
+                <div class="row no-gutters align-items-center">
+                    <div class="col-md-4">
+                        <img src="${data.urlToImage}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="h5 card-title">${data.title}</h3>
+
+                            <p class="card-text mb-4">
+                                ${data.description}
+                            </p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <span class="card-text small">
+                                    <span class="far fa-calendar-alt mr-2">
+
+                                    </span>
+                                    ${data.source.name}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        `
+        // console.log(z);
+    }
+    document.getElementById("holder").innerHTML = str;
+})
